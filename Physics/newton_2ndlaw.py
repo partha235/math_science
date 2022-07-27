08ce5919fcf9b1916518bc2346815ad9cef0830b
Parthasarathy <parthaa235@gmail.com>
2022-07-27 20:26:50 +0530

    working

---
+++ Physics/newton_2ndlaw.py
@@ -0,0 +1,10 @@
+class new_2ndlaw:
+    def force(mass,acceleration):
+        return mass*acceleration
+
+    def mass(force,acceleration):
+        return acceleration/force
+
+    def acceleration(mass,force):
+        return mass/force
+
